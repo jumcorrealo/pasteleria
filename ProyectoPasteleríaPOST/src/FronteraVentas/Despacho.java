a98cb2ee1b2d91a8d2da884fd8f9b5ea8746c42c
Juan Correa <jumcorrealo@unal.edu.co>
2019-11-05 20:15:37 -0500

    Gestion De Ventas
    Solucionado e implementado parte gráfica

--- ProyectoPasteleríaPOST/src/FronteraVentas/Despacho.java
+++ ProyectoPasteleríaPOST/src/FronteraVentas/Despacho.java
@@ -9,12 +9,12 @@ package FronteraVentas;
  *
  * @author Home
  */
-public class pedidos extends javax.swing.JFrame {
+public class Despacho extends javax.swing.JFrame {
 
     /**
      * Creates new form pedidos
      */
-    public pedidos() {
+    public Despacho() {
         initComponents();
         this.setLocationRelativeTo(null);
     }
@@ -72,20 +72,20 @@ public class pedidos extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(pedidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Despacho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(pedidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Despacho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(pedidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Despacho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(pedidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Despacho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new pedidos().setVisible(true);
+                new Despacho().setVisible(true);
             }
         });
     }
